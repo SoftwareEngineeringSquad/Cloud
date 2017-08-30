9d98c953e6896a3a4af33858cee315a89c689373
Todd Sutter <tsuttert@gmail.com>
2017-08-30 18:41:45 -0500

    use gem for canonical host instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Force Domain name in production
-  Rails.application.routes.default_url_options[:host] = 'software-squad.com'
 end
