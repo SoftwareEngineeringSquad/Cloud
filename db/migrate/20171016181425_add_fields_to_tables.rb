a0d1015c6f606e4f8722aa9a20590e2f54a46ff8
Todd Sutter <tsuttert@gmail.com>
2017-10-16 13:25:38 -0500

    items Table cols

--- db/migrate/20171016181425_add_fields_to_tables.rb
+++ db/migrate/20171016181425_add_fields_to_tables.rb
@@ -1,4 +1,10 @@
 class AddFieldsToTables < ActiveRecord::Migration
   def change
+    add_column :items, :name, :string, :null=> false
+    add_column :items, :price, :float, :null=> false, :default => 0.0
+    add_column :items, :description, :string, :null=> false, :default => "This item has no description."
+    add_column :items, :location_id, :integer, :null => false
+    add_column :items, :category_id, :integer, :null => false
+    add_column :items, :user_id, :integer, :null => false
   end
 end
