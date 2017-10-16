4d1f8e42316ee68fa466b9a6fd456fe625c9c4ea
Quinn Nelson <qnelson@unomaha.edu>
2017-10-16 13:24:15 -0500

    add columns to cateorgy and location

--- db/migrate/20171016181425_add_fields_to_tables.rb
+++ db/migrate/20171016181425_add_fields_to_tables.rb
@@ -1,4 +1,10 @@
 class AddFieldsToTables < ActiveRecord::Migration
   def change
+    add_column :category, :name, :string, :null => false
+    add_column :location, :city, :string, :null => false
+    add_column :location, :state, :string, :null => false
+
+
+
   end
 end
